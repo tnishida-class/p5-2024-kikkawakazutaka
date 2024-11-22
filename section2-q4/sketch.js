623541a93ead6cec02dd9c66ac80a0798f436bb0
kikkawakazutaka <kazu0810trp@gmail.com>
2024-11-22 13:32:10 +0900

    finalkork シューティングゲーム(クソゲー)

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -3,14 +3,23 @@ function setup() {
   const blue = color(0, 51, 160);
   createCanvas(270, 180);
   // noStroke();
+  noStroke();
   background(255);
 
   let d = height / 9; // 縞1本の太さ
 
   for(let i = 0; i < 9; i++){
     // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
+    if(i%2===0){
+      fill(blue);
     rect(0, i * d, width, (i + 1) * d);
+    } else {
+      fill(255)
+    rect(0, i * d, width, (i + 1) * d);
+    }
+
   }
+    
 
   fill(blue);
   let size = d * 5;
@@ -18,4 +27,6 @@ function setup() {
 
   fill(255);
   // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
+  rect(0, d*2, size, d)
+  rect(d*2, 0, d, size)
 }
